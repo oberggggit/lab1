feb25295fbd0b0b76d3c4ae06b1f435a3b949889
Gustav <gusoberg@gmail.com>
2024-01-17 09:27:56 +0100

    add comment

--- Saab95.java
+++ Saab95.java
@@ -2,6 +2,8 @@ import java.awt.*;
 
 public class Saab95{
 
+    //hallå där
+
     public boolean turboOn;
     public int nrDoors; // Number of doors on the car
     public double enginePower; // Engine power of the car
